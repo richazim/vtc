983e193064fd8835f7aba6269f08ce45e3d99f17
richazim <saibouazim90@gmail.com>
2025-03-14 23:33:52 +0000

    Fix For NextJs

---
+++ pages/About.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const About = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default About;
---
+++ pages/Services.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Services = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Services;
