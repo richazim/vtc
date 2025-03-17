971e8de4a80c1c643697de700154572379c5a163
richazim <saibouazim90@gmail.com>
2025-03-17 16:05:21 +0000

    Home Rotating Words

---
+++ components/HomeRotatingText.tsx
@@ -0,0 +1,59 @@
+"use client";
+import {useEffect, useRef} from "react";
+import { gsap } from "gsap";
+
+export default function HeroSection() {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const wordsRef = useRef<HTMLDivElement[]>([]);
+
+    useEffect(() => {
+        const container = containerRef.current;
+        const words = wordsRef.current;
+        const wordHeight = container!.clientHeight / words.length;
+
+        const timeline = gsap.timeline({ // Créer une timeline d'animations où chaque animation est enchaînée.
+            repeat: -1,
+            defaults: { ease: "power2.inOut" }
+        });
+
+        words.forEach((item, index) => {
+            if(index < words.length - 1){
+                timeline.to(container, { // Etat final A Atteindre
+                    y: `-=${wordHeight}px`,
+                    duration: 1
+                });
+            }
+        });
+
+        timeline.to(container, {
+            y: `0`,
+            duration: 1
+        });
+    }, []);
+
+    return (
+        <div className="relative h-40 flex items-center justify-center h-margin-left w-full">
+            <h1 className="xl:text-3xl lg:text-2xl">
+                MrVTC, c’est{" "}
+
+                <span className="relative inline-block overflow-hidden h-8">
+                  <div ref={containerRef} className="container flex flex-col items-start  text-3xl">
+                    {["LE CONFORT", "LA RAPIDITÉ", "LE RESPECT", "LA PONCTUALITÉ", "LA GARANTIE"].map((word, index) => (
+                        <div
+                            key={word}
+                            ref={(el) => {
+                                if(el !== null) {
+                                    wordsRef.current[index] = el;
+                                }
+                            }}
+                            className={`rotating-word text-left font-bold inline-block`}
+                        >
+                            {word}
+                        </div>
+                    ))}
+                  </div>
+                </span>
+            </h1>
+        </div>
+    );
+}
--- pages/Welcome.tsx
+++ pages/Welcome.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import HomeRotatingText from "@/components/HomeRotatingText";
 
 const Home = () => {
     return (
         <header className="w-full h-screen flex flex-row overflow-hidden">
-            <section className="lg:w-[50%]">
-
+            <section className="lg:w-[50%] lg:flex hidden lg:flex-row lg:items-center bg-[#F7F4ED]">
+                {/*<p className="text-[50px] font-bold">VTC SANS LIMITE</p>*/}
+                <HomeRotatingText/>
             </section>
 
             <section className="relative h-screen w-screen lg:w-[50%] -z-10 bg-origin-border">
