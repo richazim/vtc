74cfc71fe5b41dca163b2121ca65615fa6ada5e6
richazim <saibouazim90@gmail.com>
2025-03-20 23:43:15 +0000

    Add Social Medias

---
+++ components/SocialMedias.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import Image from "next/image";
+
+const SocialMedias = () => {
+    return (
+        <div className="flex flex-row gap-[15px]">
+            <Image src="/assets/icons/twitter.svg" alt="twitter icon" width={24} height={24} />
+            <Image src="/assets/icons/facebook.svg" alt="facebook icon" width={24} height={24} />
+            <Image src="/assets/icons/tiktok.svg" alt="tiktok icon" width={24} height={24} />
+            <Image src="/assets/icons/instagram.svg" alt="instagram icon" width={24} height={24} />
+        </div>
+    );
+};
+
+export default SocialMedias;
--- pages/About.tsx
+++ pages/About.tsx
@@ -4,6 +4,7 @@ import {gsap} from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import ManPicture from "@/public/assets/images/image1.jpg";
 import Image from "next/image";
+import SocialMedias from "@/components/SocialMedias";
 
 const About = () => {
     const aboutRef = useRef(null);
@@ -19,22 +20,25 @@ const About = () => {
     }, []);
 
     return (
-        <div ref={aboutRef} id="about" className="w-screen font-bold h-padding padding-top-nav h-screen overflow-hidden bg-black">
+        <div ref={aboutRef} id="about" className="w-screen h-padding padding-top-nav padding-bottom-nav overflow-hidden bg-black">
 
-            <div className="flex flex-row">
-                <section className="w-[50%]">
+            <div className="flex lg:flex-row flex-col gap-[30px] lg:gap-0">
+                <section className="lg:w-[50%] w-full">
                     <Image src={ManPicture} priority alt="Man" width={5760} height={3840} placeholder="blur" className="object-cover h-full w-full" />
                 </section>
 
-                <section className="w-[50%] text-white">
+                <section className="lg:w-[50%] w-full text-white flex flex-col gap-[30px] items-center">
                     {/*<p>À Propos</p>*/}
-                    <h2 className="text-center text-5xl text-yellow">Qui suis-je?</h2>
-                    <p>Chauffeur vtc privée basé à Angers Passionné par mon métier, je mets mon expérience et mon professionnalisme à votre service pour vous garantir des trajets confortables, sécurisés et agréables.</p>
-                    <p>Fort d’une solide connaissance des routes angevines et des environs, je vous assure une prise en charge ponctuelle et un itinéraire optimisé pour vos déplacements personnels ou professionnels.</p>
-                    <p>ue ce soit pour un transfert vers une gare, un aéroport ou un rendez-vous important, je veille à ce que votre voyage soit serein et sans encombre.</p>
-                    <p>Mon véhicule, soigneusement entretenu et équipé pour votre confort, vous offre une expérience de transport haut de gamme. À bord, vous trouverez divers services à votre disposition : rafraîchissements, chargeurs, connexion Wi-Fi et bien plus encore pour rendre votre trajet des plus agréables.</p>
-                    <p>Ma priorité ? Votre satisfaction et votre tranquillité d’esprit. Disponibilité, discrétion et courtoisie sont les maîtres-mots de mon engagement envers vous. Réservez votre course dès maintenant et laissez-moi vous conduire en toute sérénité.</p>
+                    <h2 className="text-center text-5xl text-yellow font-bold">Qui suis-je?</h2>
+                    <div className="h-padding  flex flex-col gap-[15px] text-gray text-[15px]">
+                        <p>Chauffeur vtc privée basé à Angers Passionné par mon métier, je mets mon expérience et mon professionnalisme à votre service pour vous garantir des trajets confortables, sécurisés et agréables.</p>
+                        <p>Fort d’une solide connaissance des routes angevines et des environs, je vous assure une prise en charge ponctuelle et un itinéraire optimisé pour vos déplacements personnels ou professionnels.</p>
+                        <p>Que ce soit pour un transfert vers une gare, un aéroport ou un rendez-vous important, je veille à ce que votre voyage soit serein et sans encombre.</p>
+                        <p>Mon véhicule, soigneusement entretenu et équipé pour votre confort, vous offre une expérience de transport haut de gamme. À bord, vous trouverez divers services à votre disposition : rafraîchissements, chargeurs, connexion Wi-Fi et bien plus encore pour rendre votre trajet des plus agréables.</p>
+                        <p>Ma priorité ? Votre satisfaction et votre tranquillité d’esprit. Disponibilité, discrétion et courtoisie sont les maîtres-mots de mon engagement envers vous. Réservez votre course dès maintenant et laissez-moi vous conduire en toute sérénité.</p>
+                    </div>
 
+                    <SocialMedias/>
                 </section>
             </div>
 
