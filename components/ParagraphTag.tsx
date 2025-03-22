a4d01b6c2f317e405f782454778ec6366d8e1533
richazim <saibouazim90@gmail.com>
2025-03-22 14:53:32 +0000

    Services

---
+++ components/ParagraphTag.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+
+const ParagraphTag = ({onColumn, title, paragraph} : {onColumn?: boolean, title: string, paragraph: string}) => {
+    return (
+        <div className={`${(onColumn) ? 'flex flex-col' : 'flex flex-row w-full'} w-full bg-gray `}>
+            <div className={`${(onColumn) ? 'w-full' : 'w-1/2'} overflow-hidden`}>
+                <Image src="/assets/images/image2.jpg" alt="" width={5760} height={3840} className="object-cover"/>
+            </div>
+
+            <div className={`${(onColumn) ? 'w-full' : 'w-1/2'} overflow-hidden`}>
+                <h4 className="text-[40px] font-bold text-center p-[40px]">{title}</h4>
+                <p className="p-[10px]">{paragraph}</p>
+            </div>
+        </div>
+    );
+};
+
+export default ParagraphTag;
--- pages/Services.tsx
+++ pages/Services.tsx
@@ -1,9 +1,32 @@
 "use client";
 
+import ParagraphTag from "@/components/ParagraphTag";
+
+const services = [
+    {
+        id: 1,
+        title: "Trajets longue distance",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus"
+    },
+    {
+        id: 2,
+        title: "Trajets longue distance",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus"
+    },
+]
+
 const Services = () => {
     return (
-        <div id="services" className="h-screen flex justify-center items-center text-9xl font-bold">
-            <p>Services</p>
+        <div id="services" className="padding-top-nav">
+            <h2 className="lg:text-[120px] text-[70px] font-bold text-center text-yellow">Services</h2>
+
+            <div className="h-padding flex flex-col gap-[20px]">
+                <ParagraphTag title={services[0].title} paragraph={services[0].paragraph}/>
+                <div className="flex flex-row gap-[20px]">
+                    <ParagraphTag onColumn={true} title={services[0].title} paragraph={services[0].paragraph}/>
+                    <ParagraphTag onColumn={true} title={services[0].title} paragraph={services[0].paragraph}/>
+                </div>
+            </div>
         </div>
     );
 };
