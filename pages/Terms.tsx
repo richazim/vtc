3018f5f64bf0ac25e9c83e0bac431ad69b2b65e2
richazim <saibouazim90@gmail.com>
2025-03-23 21:50:16 +0000

    Terms Section

--- pages/Terms.tsx
+++ pages/Terms.tsx
@@ -2,8 +2,24 @@
 
 const Terms = () => {
     return (
-        <div>
-            Terms
+        <div className="mt-[50px] h-[70px] flex lg:flex-row pt-[10px] lg:pt-0 flex-col items-center justify-between h-padding bg-[#00171f] text-white text-[12px]">
+            <p className="">
+                <span>
+                    © Copyright - 2025 - Site réalisé par 
+                </span>
+
+                <span className="border rounded-[7px] p-[5px]">
+                    Azim et Badjo
+                </span>
+            </p>
+
+            <div className="flex flex-row gap-[20px]">
+                <span>Véhicules</span>
+                <span>Prestations</span>
+                <span>Couverture sur toute la France</span>
+                <span>Mentions Légales</span>
+                <span>Politique de confidentialité</span>
+            </div>
         </div>
     );
 };
